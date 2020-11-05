10a8eda1d323c8ac30dc51a214c2d967d0ffe7fc
Amused_Pan <pieter@joyjet.com>
2020-11-05 10:23:27 +0100

    Added Dotween

---
+++ Assets/SpinTween.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpinTween : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 0;
+
+    private void Awake()
+    {
+        this.transform.DORotate(new Vector3(0, 360, 0), rotationSpeed, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
+    }
+}
